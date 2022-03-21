192aa7ad2008d66181362e3c14ec5f7d4e7c85d2
AlejandroRuizZ <a01611451@itesm.mx>
2022-03-21 16:08:35 -0600

    Avance de Programa Lexer lee variables, Op, y commentarios.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 
 using namespace std;
 
-void lexerAritmetico(string archivo) {
-  // to do
+void lexer(string archivo) {
+  ifstream input; //file
+  char current; //current character
+  input.open(archivo); //open file to read
+  current =' '; //current character starts as empty
+  string number = "";
+  string var = "";
+
+  while(input.get(current)){//while you can get a character from file
+    if(current == '+'){
+      cout << current << " " << "Operador de Suma" << endl;
+    }
+    if(current == '*'){
+      cout << current << " " << "Operador de Multiplicacion" << endl;
+    }
+    if(current == '('){
+      cout << current << " " << "Caracter Especial" <<endl;
+    }
+    if(current == ')'){
+      cout << current << " " << "Caracter Especial" <<endl;
+    }
+    if(current == '^'){
+      cout << current << " " << "Operador de Potencia" << endl;
+    }
+    if(current == '='){
+      cout << current << " " << "Operador de Igual" << endl;
+    }
+    if(isdigit(current)){
+      number = number + current;
+    }
+    if(isalpha(current)){
+
+      var = var + current;
+    }
+      }
+      cout << var << " " << "Variable" << endl;
+      cout << number << " " << "Numero Entero" <<endl;
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cout << "usage: " << argv[0] << " pathname\n";
-    return -1;
-  }
 
-  lexerAritmetico(argv[1]);
 
+
+
+
+
+
+int main() {
+  lexer("input1.txt");
   return 0;
 }
