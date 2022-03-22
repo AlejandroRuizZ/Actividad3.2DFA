097bfee5076ce4f263cbd4681c78d75bbe706934
AlejandroRuizZ <a01611451@itesm.mx>
2022-03-21 22:06:26 -0600

    Lexer terminado

--- main.cpp
+++ main.cpp
@@ -12,9 +12,23 @@ void lexer(string archivo) {
   input.open(archivo); //open file to read
   current =' '; //current character starts as empty
   string number = "";
-  string var = "";
+  string var = " ";
+  
+  while(input.get(current)){//while you can get a character from file 
+    char c = input.peek();
 
-  while(input.get(current)){//while you can get a character from file
+    if (current =='/' and c == '/'){
+      var=current;
+    }
+    if ( var.at(0) == '/' and current != '\n'){
+      var=var+current;
+    }
+    if (current == '\n' and var.at(0) == '/'){
+      var.erase(0,1);
+      cout << var << " " << "Comentario" << endl;
+      var = " ";
+    }
+    
     if(current == '+'){
       cout << current << " " << "Operador de Suma" << endl;
     }
@@ -33,24 +47,73 @@ void lexer(string archivo) {
     if(current == '='){
       cout << current << " " << "Operador de Igual" << endl;
     }
-    if(isdigit(current)){
-      number = number + current;
+    if(current == '-' and c == ' '){
+      cout << current << " " << "Operador de Resta" <<endl;
     }
-    if(isalpha(current)){
-
-      var = var + current;
+    
+    if(isdigit(c) and current == '-' ){
+      number= number + current ;
     }
+    if(isdigit(current) ){
+      number= number + current;
+      if (c == '.'|| c=='E' || c=='e'){
+        number= number + c ;
       }
-      cout << var << " " << "Variable" << endl;
-      cout << number << " " << "Numero Entero" <<endl;
+      if (c==' ' || c=='\n'){
+        size_t t1= number.find('.');
+        size_t t2= number.find('e');
+        size_t t3= number.find('E');
+        if (t1 == string::npos ||t2 == string::npos ||t3 == string::npos){
+          cout << number << " " << "Numero Real" <<endl;
+          number="";
+        }
+        if ((t1 || t2 || t3)==false){
+          cout << number << " " << "Numero Entero" <<endl;
+          number="";
+        }
+      }
+
+    }
+
+
+    if(isdigit(current) || isalpha(current)||input.eof() ){
+        if(isalpha(current) and var.at(0) == ' '){
+            var = current;
+        }
+
+        if (isalpha(var.at(0))){
+            if(isalpha(current)){
+            var = var + current;
+            }
+            if(isdigit(current) ){
+            var = var + current;
+            } 
+            if (input.eof() || c == ' ' ||  c == '+' || c == '-' || c == '*' || c == '/' || c == '>' || c == '<' ||
+            c == '=' || c == '/' || c == '(' || c== ')') {
+                var.erase(0,1);
+                cout << var << " " << "Variable" << endl;
+                var = " ";
+            };
+
+        }
+
+    }  //Esto es un commentario Commentario
+      //number = number + current; 
+  }
+    /*if(isalpha(current)){
+        var = var + current;
+        if ( current == ' ')
+        {
+            cout << var << " " << "Variable" << endl; 
+            var= "";
+        }
+        
+       
+      
+    }
+    */
+    //cout << var << " " << "Numero Entero" <<endl;
 }
-
-
-
-
-
-
-
 
 int main() {
   lexer("input1.txt");
